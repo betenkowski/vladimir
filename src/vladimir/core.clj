3bf1050ca76327fd1bbec6236ce310383dd3ace2
Bartek Etenkowski <bartek@sumologic.com>
2018-11-18 02:55:52 +0100

    migrated to ring

--- src/vladimir/core.clj
+++ src/vladimir/core.clj
@@ -1,11 +1,29 @@
 (ns vladimir.core
-  (:require [org.httpkit.server :refer [run-server]]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.handler :refer [api]]
+            [compojure.route :refer [not-found]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :as middleware]))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body "{ \"text\" : \"Wombats rule! Koalas are cute!\" }"})
+(def show-info
+  {:body {:info "All wombats play Putin!"}})
 
-(defn -main [& args]
-  (run-server app {:port 8080})
-  (println "Vladimir started on port 8080"))
+(defn create-game []
+  {:body {:id "wombat"}})
+
+(defn get-game-log [id]
+  {:body {:id id :log ["wombat" "koala" "hedgehod"]}})
+
+(defroutes app-routes
+           (GET "/" [] show-info)
+           (POST "/games" [] (create-game))
+           (GET "/games/:id/log" [id] (get-game-log id))
+           (not-found "Not found"))
+
+(def handler
+  (-> (api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-params)
+      (middleware/wrap-json-response)))
+
+(defn -main [& _] (run-jetty handler {}))
